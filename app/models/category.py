281d0ebaae2efa153d83f41d000c7e85dc367c44
Ayjemal Berdiyeva <ayjemal908@outlook.com>
2025-08-04 08:08:55 +0200

    Update .gitignore to ignore __pycache__ and IDE files

---
+++ app/models/category.py
@@ -0,0 +1,12 @@
+from app.models import db
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+
+    # Связь с вопросами
+    questions = db.relationship('Question', backref='category', lazy=True)
+
+    def __repr__(self):
+        return f'Category: {self.name}'
